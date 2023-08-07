a5355056f530d8f2d206d8d1532c9c741c3afaf3
root <ninthflooor@gmail.com>
2023-08-07 17:27:50 +0900

    add name attribute"

--- app/form/ControlledComponent.jsx
+++ app/form/ControlledComponent.jsx
@@ -1,23 +1,34 @@
+/*
+1. 이용자가 페이지를 본다
+2. 3개 폼을 입력한다. (모두 필수임)
+3. reset 을 클릭하면 페이지가 리셋된다
+4. submit 을 클릭한다.
+5. 입력완료 메시지가 뜬다.
+*/
+
 export const ControlledComponent = () => {
   return (
     <form>
       <div>
-        <label>name
-        <input type="text"/>
+        <label>
+          name
+          <input type="text" required={true} name='name' placeholder="이름"  />
         </label>
       </div>
       <div>
-        <label>profile
-        <input type="text"/>
+        <label>
+          profile
+          <textarea type="text" required={true} name='profile' placeholder="간단한 소개" />
         </label>
       </div>
       <div>
-        <label>essay
-        <textarea type="text"/>
+        <label>
+          essay
+          <textarea type="text" required={true} name='essay' placeholder="에세이" />
         </label>
       </div>
       <button>submit</button>
       <button>reset</button>
     </form>
-  )
-}
+  );
+};
